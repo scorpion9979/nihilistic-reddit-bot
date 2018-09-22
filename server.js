c23e73d360786897eccac7ead3add0f1e8467ba7
scorpion9979 <you@example.com>
2018-09-22 17:57:40 +0300

    Employed sentiment package to seek comments with negative sentiment

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const Snoowrap = require("snoowrap");
 const Snoostorm = require("snoostorm");
+const Sentiment = require("sentiment");
 const replies = require("./replies");
 
 // setting up clients
@@ -22,13 +23,25 @@ const streamOpts = {
 // create comment stream with given options
 const comments = client.CommentStream(streamOpts);
 
+// create a new sentiment
+const sentiment = new Sentiment();
+
 // perform callback for every comment
 comments.on("comment", (comment) => {
-    if (comment.body === ":(") {
+    let metricThreshold = -20;
+    let wordCountThreshold = 50;
+    let result = sentiment.analyze(comment.body);
+    let metric = result.score + result.comparative;
+    let wordCount = result.tokens.length;
+    if (metric < metricThreshold && wordCount < wordCountThreshold) {
         let randomIndex = Math.floor(Math.random() * replies.length);
-        comment.reply(replies[randomIndex])
-               .catch(function() {
-                   console.log("Rate limit reached, hang in there...");
+        let reply = replies[randomIndex];
+        console.log("Replying to: \"" + comment.body + "\"\nwith: \"" + reply + "\"");
+        comment.reply(reply + "\n"
+                      + "---" + "I'm a bot created by /u/scorpion9979 | "
+                      + "[source code](https://github.com/scorpion9979/nihilistic-reddit-bot)")
+               .catch(function(err) {
+                   console.log(err);
                 });
     }
 });
