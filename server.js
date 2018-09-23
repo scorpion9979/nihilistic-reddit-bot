384e9811d997fdc5e1a738710a439488d8e106a1
scorpion9979 <you@example.com>
2018-09-23 16:48:20 +0300

    Refactored log and improved error handling

--- server.js
+++ server.js
@@ -36,12 +36,12 @@ comments.on("comment", (comment) => {
     if (metric < metricThreshold && wordCount < wordCountThreshold) {
         let randomIndex = Math.floor(Math.random() * replies.length);
         let reply = replies[randomIndex];
-        console.log("Replying to: \"" + comment.body + "\"\nwith: \"" + reply + "\"");
+        console.log("\nReplying to: \"" + comment.body + "\"\nwith: \"" + reply + "\"\n");
         comment.reply(reply + "\n"
                       + "\n---\n" + "I'm a bot created by \\/u/scorpion9979 | "
                       + "[source code](https://github.com/scorpion9979/nihilistic-reddit-bot)")
                .catch(function(err) {
-                   console.log(err);
+                   console.log("\nCaught error: " + err.message + "\nHang tight!");
                 });
     }
 });
